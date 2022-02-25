95c802c339739071636a364a867dcaee49d3803e
Carl Skoglund <carl.skoglund@ica.se>
2022-02-25 12:37:26 +0100

    Create start workout button

--- workoutAppAndroid/src/main/java/com/onex/clever/workout/android/MainActivity.kt
+++ workoutAppAndroid/src/main/java/com/onex/clever/workout/android/MainActivity.kt
@@ -2,6 +2,7 @@ package com.onex.clever.workout.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.onex.clever.workout.Greeting
 import android.widget.TextView
 
@@ -13,8 +14,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+
+        val start_button = findViewById<Button>(R.id.start_workout)
+        start_button.setOnClickListener{
+            tv.text = greet()
+        }
+
+
+
     }
 }
