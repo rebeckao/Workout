8a5bf125be05cea6d1a902a26ca632c64d5d7df5
Ulf Hedlund <ulf.hedlund@ica.se>
2022-02-25 13:15:16 +0100

    Refactor

--- workoutAppAndroid/src/main/java/com/onex/clever/workout/android/WorkoutSessionActivity.kt
+++ workoutAppAndroid/src/main/java/com/onex/clever/workout/android/WorkoutSessionActivity.kt
@@ -3,13 +3,21 @@ package com.onex.clever.workout.android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.onex.clever.workout.ExcerciseRandomizer
 
 class WorkoutSessionActivity : AppCompatActivity() {
+    var currentExcerciseIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_workout_session)
-        val exerciseName: TextView = findViewById(R.id.exerciseName)
-        val exerciseDescription: TextView = findViewById(R.id.description)
+        val exerciseName = findViewById<TextView>(R.id.exerciseName)
+        val exerciseDescription = findViewById<TextView>(R.id.description)
 
+        val randomizer = ExcerciseRandomizer()
+        val session = randomizer.createWorkoutSession()
+
+        exerciseName.text = session[currentExcerciseIndex].name
+        exerciseDescription.text = session[currentExcerciseIndex].description
     }
 }
