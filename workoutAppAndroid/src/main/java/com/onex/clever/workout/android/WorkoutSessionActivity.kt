6f66a8df26b7255b6c540c0cbdc9cd80e78fe714
Rebecka Öfverholm <rebecka.ofverholm@ica.se>
2022-02-25 13:27:40 +0100

    Button to go to next exercise

--- workoutAppAndroid/src/main/java/com/onex/clever/workout/android/WorkoutSessionActivity.kt
+++ workoutAppAndroid/src/main/java/com/onex/clever/workout/android/WorkoutSessionActivity.kt
@@ -2,7 +2,9 @@ package com.onex.clever.workout.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import com.onex.clever.workout.Excercise
 import com.onex.clever.workout.ExcerciseRandomizer
 
 class WorkoutSessionActivity : AppCompatActivity() {
@@ -13,10 +15,23 @@ class WorkoutSessionActivity : AppCompatActivity() {
         setContentView(R.layout.activity_workout_session)
         val exerciseName = findViewById<TextView>(R.id.exerciseName)
         val exerciseDescription = findViewById<TextView>(R.id.description)
+        val nextButton = findViewById<Button>(R.id.next_button)
 
         val randomizer = ExcerciseRandomizer()
         val session = randomizer.createWorkoutSession()
 
+        displayNextExercise(exerciseName, session, exerciseDescription)
+        nextButton.setOnClickListener {
+            currentExcerciseIndex++
+            displayNextExercise(exerciseName, session, exerciseDescription)
+        }
+    }
+
+    private fun displayNextExercise(
+        exerciseName: TextView,
+        session: List<Excercise>,
+        exerciseDescription: TextView
+    ) {
         exerciseName.text = session[currentExcerciseIndex].name
         exerciseDescription.text = session[currentExcerciseIndex].description
     }
