f020514bdbf39a3c4cc1e829a0b06c34b4ea875b
Adam Heidmark <adam.heidmark@ica.se>
2022-02-25 14:02:28 +0100

    Restart

--- workoutAppAndroid/src/main/java/com/onex/clever/workout/android/SuccessActivity.kt
+++ workoutAppAndroid/src/main/java/com/onex/clever/workout/android/SuccessActivity.kt
@@ -1,11 +1,19 @@
 package com.onex.clever.workout.android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SuccessActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_success)
+
+        val restart_button = findViewById<Button>(R.id.restart)
+        restart_button.setOnClickListener{
+            val intent = Intent(this, WorkoutSessionActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
